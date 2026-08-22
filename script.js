395e07d314d0c4a3ed722ee392ad92ad1908d56b
johnsonmulbahe-oss <johnsonmulbahe@gmail.com>
2026-08-22 18:33:13 +0000

    Load New Services CSS externally for CSP compatibility

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 (function(){
+  const css=document.createElement('link');
+  css.rel='stylesheet';
+  css.href='new-services.css?v=20260822cspfix';
+  document.head.appendChild(css);
+
   const core=document.createElement('script');
-  core.src='script-core.js?v=20260822';
+  core.src='script-core.js?v=20260822cspfix';
   core.onload=function(){
     const extra=document.createElement('script');
-    extra.src='new-services.js?v=20260822';
+    extra.src='new-services.js?v=20260822cspfix';
     document.body.appendChild(extra);
   };
   document.body.appendChild(core);
