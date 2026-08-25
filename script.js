f50e5663aa00c2f2a1d4f7680c0dc02afa562083
johnsonmulbahe-oss <johnsonmulbahe@gmail.com>
2026-08-25 23:20:54 +0000

    Refresh flyer viewer assets in production

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 (function(){
   const serviceCss=document.createElement('link');
   serviceCss.rel='stylesheet';
-  serviceCss.href='new-services.css?v=20260822cspfix';
+  serviceCss.href='new-services.css?v=20260825flyerviewer';
   document.head.appendChild(serviceCss);
 
   const mobileCss=document.createElement('link');
   mobileCss.rel='stylesheet';
-  mobileCss.href='mobile-nav.css?v=20260822menu1';
+  mobileCss.href='mobile-nav.css?v=20260825menu';
   document.head.appendChild(mobileCss);
 
   const core=document.createElement('script');
-  core.src='script-core.js?v=20260822cspfix';
+  core.src='script-core.js?v=20260825core';
   core.onload=function(){
     const menu=document.createElement('script');
-    menu.src='mobile-nav.js?v=20260822menu1';
+    menu.src='mobile-nav.js?v=20260825menu';
     document.body.appendChild(menu);
 
     const extra=document.createElement('script');
-    extra.src='new-services.js?v=20260822cspfix';
+    extra.src='new-services.js?v=20260825flyerviewer';
     document.body.appendChild(extra);
   };
   document.body.appendChild(core);
